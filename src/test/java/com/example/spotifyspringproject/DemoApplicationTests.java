2b08f902c90dce21cfb9530e946c60554caf0c1f
Sailesh Patel <saileshapatel@gmail.com>
2020-05-19 15:37:23 +0100

    initial commit

---
+++ src/test/java/com/example/spotifyspringproject/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifyspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

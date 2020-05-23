6922ac009141588986cc25d39a22fe7fc3465574
Sailesh Patel <saileshapatel@gmail.com>
2020-05-23 19:17:56 +0100

    move /code/spotify endpoint to IndexController

--- src/main/java/com/spotify/project/controllers/IndexController.java
+++ src/main/java/com/spotify/project/controllers/IndexController.java
@@ -4,7 +4,9 @@ import com.spotify.project.authorisation.SpotifyAuth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -16,5 +18,13 @@ public class IndexController {
     public String index(Model model){
         model.addAttribute("auth", spotifyAuth.getAuthorisationUri());
         return "index";
+    }
+
+    @GetMapping("/code/spotify")
+    public String authCode(@RequestParam(required = false) String code,
+                           @RequestParam(required = false) String state,
+                           @RequestParam(required = false) String error,
+                           Model model) {
+        return "auth";
     }
 }
